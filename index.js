f07737d0b4b7b578e7919c622a77bd149432467f
baochun <baochun.xie@ericsson.com>
2019-05-24 17:06:29 +0800

    initialize the project

---
+++ index.js
@@ -0,0 +1,32 @@
+const Koa = require('koa');
+const render = require('koa-ejs');
+const path = require('path');
+const serve = require('koa-static');
+const router = require('./src/router/index.js');
+var sass = require('koa-sass');
+
+const app = new Koa();
+
+app.use(sass({
+  src:  __dirname + '/src/assets/scss',
+  dest: __dirname + '/src/assets/',
+  debug: true
+}));
+
+app.use(serve(path.join(__dirname, 'src/assets')));
+
+render(app, {
+	root: path.join(__dirname, 'src/views'),
+	layout: 'layout',
+	viewExt: 'html',
+	cache: false,
+	debug: true
+});
+
+app.use(router.middleware())
+
+// app.use(async ctx => {
+//   await ctx.render('index', {user: 'username'});
+// });
+
+app.listen(3001);
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+const Router = require('koa-trie-router');
+
+let router = new Router();
+
+router
+  .use(function(ctx, next) {
+    console.log('* requests')
+    return next()
+  })
+  .get(function(ctx, next) {
+    console.log('GET requests')
+    return next()
+  }).
+  get('/', async (ctx, next) => {
+  	await ctx.render('pages/index', {user: 'username'})
+  	//return next()
+  })
+  .put('/foo', function (ctx) {
+    ctx.body = 'PUT /foo requests'
+  })
+  .post('/bar', function (ctx) {
+    ctx.body = 'POST /bar requests'
+  })
+module.exports = router
